4358c3f3b192df30bd5f56e42671004f98773dcb
Cláudio Tomás <ccaducarlos@gmail.com>
2023-03-13 20:08:52 +0200

    modal  opens and closes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import logo from "../../assets/logo.svg"
+import ReactModal from "react-modal";
 import { Container, Content } from "./styles";
 
 
 export function Header() {
+	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+	function handleOpenNewTransactionModal() {
+		setIsNewTransactionModalOpen(true);
+	}
+
+	function handleCloseNewTransactionModal() {
+		setIsNewTransactionModalOpen(false)
+	}
+
 	return (
 		<Container>
 			<Content>
 				<img src={logo} alt="dt money" />
-				<button type="button">Nova transação</button>
+				<button type="button" onClick={handleOpenNewTransactionModal}>Nova transação</button>
+
 			</Content>
+			<ReactModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+				<h2>Cadastrar transação</h2>
+			</ReactModal>
 		</Container>
 	);
 }
